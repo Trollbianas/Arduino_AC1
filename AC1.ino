7a5be57a59014010e70ed7d676dc9a3506a624db
ananerim <81580941+ananerim@users.noreply.github.com>
2021-04-01 18:13:14 -0300

    Update AC1.ino

--- AC1.ino
+++ AC1.ino
@@ -1,11 +1,12 @@
 //variaveis da led
 const int vermelho = 5;
 const int verde = 6;
-const int azul = 7
+const int azul = 7;
 
 bool estadoLedVermelho = false;
 
 const int botao1 = 2;
+const int botao2 = 3;
 unsigned long lastDebounceTime1 = 0;
 const int botaoDelay = 100;
 
@@ -16,42 +17,57 @@ void setup()
   
   pinMode(vermelho, OUTPUT);
   pinMode(verde, OUTPUT);
-  pinMode(azul, saida);
+  pinMode(azul, OUTPUT);
   
   Serial.begin(9600);
 	
   Serial.println("AC1 - Meu Primeiro Projeto 2021");
-  Serial.println("                           V1.0");
-  Serial.println("Grupo: NOME AQUI               ");
+  Serial.println("V1.0");
+  Serial.println("Grupo: TROLLBIANAS");
 }
 
 void loop()
 {
   if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao1)){
-  	Serial.println("botao 1 apertado");
-    ledVermelho();
+  	Serial.println("início prod");
+    ledVermelho(true);
   	lastDebounceTime1 = millis();
   }
   
+ if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao2)){
+	Serial.Sprintln("final prod");
+    ledVermelho(false);
+	 lastDebounceTime2 = millis();
+ }
+	
   //verificador de tempetura
-  if(getTemperatura() > 30){
+  if(getTemperatura() > 15){
     ledAzul(true);
+        Serial.Sprintln("temperatura além do planejado");
   }else{
-  	ledAzul(falso); 
+  	ledAzul(false); 
+       Serial.Sprintln("temperatura planejada");
   }
-  	
+	
+  if(getLuminosidade() > 5){
+        ledVerde(true);
+       Serial.Sprintln("luminosidade além do planejado");
+  }else{
+	ledVerde(false);
+       Serial.Sprintln("luminosidade planejada");
+  }  
+  
   delay(10);
 }
 
-void ledVermelho(){
-  estadoLedVermelho = !estadoLedVermelho;
-  digitalWrite(vermelho,estadoLedVermelho);
+void ledVermelho(bool estado){
+  digitalWrite(vermelho,estado);
 }
-void ledVerde(){
- //precisa fazer  
+void ledVerde(bool estado){
+  digitalWrite(verde,estado);
 }
 void ledAzul(bool estado){
-	digitalWrite(azul,estado);
+ digitalWrite(azul,estado);
 }
 
 int getTemperatura(){
